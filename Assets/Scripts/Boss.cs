2ac0038dc984dff2886c880bd26d98f8e5017119
sid-shakthivel <sid.shakthivel@gmail.com>
2021-08-21 20:44:33 +0100

    Cleaned Procedural Generation

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -4,35 +4,7 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    private DungeonGenerator DungeonGeneratorScript;
-    private GameObject PlayerObject;
-
-    private Vector2 Target;
-
     private void Awake()
     {
-        GameObject DungeonGeneratorObject = GameObject.FindGameObjectWithTag("DungeonGenerator");
-        PlayerObject = GameObject.FindGameObjectWithTag("Player");
-        DungeonGeneratorScript = DungeonGeneratorObject.GetComponent<DungeonGenerator>();
-
-        
-    }
-
-    private void DoThing()
-    {
-        foreach (var Item in DungeonGeneratorScript.DungeonHashMap)
-            Target = Item.Key;
-    }
-
-    private float GetShortestPath(List<DungeonNode> Dungeons)
-    {
-        foreach (var Item in Dungeons)
-        {
-            if (Item.Position == Target)
-                return Item.Distance;
-            else
-                return Item.Distance + GetShortestPath(DungeonGeneratorScript.DungeonHashMap[Item.Position]);
-        }
-        return 0;
     }
 }
--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -2,60 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DungeonNode {
-    public float Distance;
-    public Vector2 Position;
-
-    public DungeonNode(float NewDistance, Vector2 NewPosition)
-    {
-        Distance = NewDistance;
-        Position = NewPosition;
-    }
-}
-
 public class DungeonGenerator : MonoBehaviour
 {
-    // Rewrite All This...
     public GameObject Dungeon;
     public GameObject Path;
-    public IDictionary<Vector2, List<DungeonNode>> DungeonHashMap = new Dictionary<Vector2, List<DungeonNode>>();
 
     private int[,] Grid = new int[5, 5];
     private Vector2 OldDungeon;
-    private Vector2 Target;
-    private Vector2 OldItem;
 
     private void Start()
     {            
         CreateDungeons();
         CreatePaths();
-        DoThing();
-        //PrintGrid();
-    }
-
-    private void DoThing()
-    {
-        foreach (var Item in DungeonHashMap)
-            Target = Item.Key;
-        float Distance = GetShortestPath(DungeonHashMap[new Vector2(0, 0)]);
-        Debug.Log("Distance To " + Target + "Is " + Distance);
-    }
-
-    private float GetShortestPath(List<DungeonNode> Dungeons)
-    {
-        foreach (var Item in Dungeons)
-        {
-            if (Item.Position == OldItem)
-                continue;
-            else if (Item.Position == Target)
-                return Item.Distance;
-            else
-            {
-                OldItem = Item.Position;
-                return Item.Distance + GetShortestPath(DungeonHashMap[Item.Position]);
-            }
-        }
-        return 0;
     }
 
     private void CreateDungeons()
@@ -77,8 +35,23 @@ public class DungeonGenerator : MonoBehaviour
             {
                 DungeonCount += 1;
                 CreateDungeon(NewDungeon);
-            } 
+            }
         }
+    }
+
+    private void CreatePath(Vector2 Position1, Vector2 Position2)
+    {
+        Position1 = GetDungeonPosition(Position1);
+        Position2 = GetDungeonPosition(Position2);
+        Vector2 Position = (Position1 + Position2) / 2;
+
+        if (Position1.x == Position2.x)
+            Path.transform.localScale = new Vector3(5f, ((Position1 - Position2).y + 10), 0);
+
+        if (Position1.y == Position2.y)
+            Path.transform.localScale = new Vector3(5f, ((Position1 - Position2).x + 10), 0);
+
+        Instantiate(Path, Position, Position1.y == Position2.y ? Quaternion.identity : Quaternion.Euler(0, 0, 90));
     }
 
     private void CreateDungeon(Vector2 DungeonVector)
@@ -86,86 +59,13 @@ public class DungeonGenerator : MonoBehaviour
         GameObject NewDungeon = Instantiate(Dungeon, GetDungeonPosition(DungeonVector), Quaternion.identity);
         NewDungeon.transform.SetParent(transform);
         Grid[(int)DungeonVector.x, (int)DungeonVector.y] = 1;
-        DungeonHashMap[GetDungeonPosition(DungeonVector)] = new List<DungeonNode>();
         OldDungeon = DungeonVector;
-    }
-
-    private void CreatePaths()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            List<int> VerticalCells = new List<int>();
-            List<int> HorizontalCells = new List<int>();
-            for (int j = 0; j < 5; j++)
-            {
-                VerticalCells.Add(Grid[i, j]);
-                HorizontalCells.Add(Grid[j, i]);
-            }
-            CreatePath(VerticalCells, i, true);
-            CreatePath(HorizontalCells, i, false);
-        }
-    }
-
-    private void CreatePath(List<int> Array, int GridIndex, bool IsVertical)
-    {
-        int CurrentDungeonIndex = GetNextFilledElement(Array, 0);
-        int LastDungeonIndex;
-
-        if (CurrentDungeonIndex == -1)
-            return;
-        else
-        {
-            while (true)
-            {
-                LastDungeonIndex = CurrentDungeonIndex;
-                CurrentDungeonIndex = GetNextFilledElement(Array, LastDungeonIndex + 1);
-                if (CurrentDungeonIndex == -1)
-                    break;
-                else
-                {
-                    Vector2 LastIndexPostion = IsVertical == true ? GetDungeonPosition(new Vector2(GridIndex, LastDungeonIndex)) : InverseVector(GetDungeonPosition(new Vector2(GridIndex, LastDungeonIndex)));
-                    Vector2 IndexPosition = IsVertical == true ? GetDungeonPosition(new Vector2(GridIndex, CurrentDungeonIndex)) : InverseVector(GetDungeonPosition(new Vector2(GridIndex, CurrentDungeonIndex)));
-
-                    Vector2 Position = (IndexPosition + LastIndexPostion) / 2;
-
-                    float Size = IsVertical == true ? ((IndexPosition - LastIndexPostion).y - 10) : (IndexPosition - LastIndexPostion).x - 10;
-
-                    Path.transform.localScale = new Vector3(5f, Size, 0);
-
-                    GameObject NewPath = Instantiate(Path, Position, IsVertical ? Quaternion.identity : Quaternion.Euler(0, 0, 90));
-                    NewPath.transform.SetParent(transform);
-
-                    if (DungeonHashMap.ContainsKey(LastIndexPostion))
-                    {
-                        DungeonHashMap[LastIndexPostion].Add(new DungeonNode(Size, IndexPosition));
-                    }
-
-                    if (DungeonHashMap.ContainsKey(IndexPosition))
-                    {
-                        DungeonHashMap[IndexPosition].Add(new DungeonNode(Size, LastIndexPostion));
-                    }
-                }
-            }
-        }
-    }
-
-    private int GetNextFilledElement(List<int> Array, int Index)
-    {
-        for (int i = Index; i < Array.Count; i++)
-        {
-            if (Array[i] == 1)
-            {
-                return i;
-            }
-        }
-        return -1;
     }
 
     private Vector2 GetDungeonPosition(Vector2 GridDungeonVector)
     {
         Vector2 GridCentreVector = new Vector2(2, 2);
         Vector2 CentreVector = new Vector2(0, 0);
-
         return ((GridDungeonVector - GridCentreVector) * 20) + CentreVector;
     }
 
@@ -175,41 +75,35 @@ public class DungeonGenerator : MonoBehaviour
         return new Vector2(Random.Range(MinX, MaxX), Random.Range(MinY, MaxY));
     }
 
-    private bool CheckForHorizontalSpaceInGrid(int j)
+    private void CreatePaths()
     {
         for (int i = 0; i < 5; i++)
         {
+            int k = 1;
+            for (int j = 0; j < 5; j++)
+            {
+                if (Grid[i, j] == 1 && Grid[i, k] == 1)
+                    CreatePath(new Vector2(i, j), new Vector2(i, k));
+                if (Grid[j, i] == 1 && Grid[k, i] == 1)
+                    CreatePath(new Vector2(j, i), new Vector2(k, i));
+                k++;
+            }
+        }
+    }
+
+    private bool CheckForHorizontalSpaceInGrid(int j)
+    {
+        for (int i = 0; i < 5; i++)
             if (Grid[i, j] != 1)
                 return true;
-        }
         return false;
     }
 
     private bool CheckForVerticalSpaceInGrid(int j)
     {
         for (int i = 0; i < 5; i++)
-        {
             if (Grid[j, i] != 1)
                 return true;
-        }
         return false;
-    }
-
-    private void PrintGrid()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            string line = "";
-            for (int j = 0; j < 5; j++)
-            {
-                line += Grid[i, j];
-            }
-            Debug.Log(line);
-        }
-    }
-
-    private Vector2 InverseVector(Vector2 Vector)
-    {
-        return new Vector2(Vector.y, Vector.x);
     }
 }
